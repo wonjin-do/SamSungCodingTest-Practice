54149084b0d3fce105f4a802eb6d783a984072b7
wonjin-do <54175950+wonjin-do@users.noreply.github.com>
2019-09-02 06:02:01 +0900

    Update 우선순위큐(힙 사용).c

--- 알고리즘잡스강의정리/스택&큐 자료구조/우선순위큐(힙 사용).c
+++ 알고리즘잡스강의정리/스택&큐 자료구조/우선순위큐(힙 사용).c
@@ -36,17 +36,13 @@ struct priorityQueue {
 	int len = 1;//새로운 데이터의 위치
 
 	void push(int x) {
-		data[len++] = x;
-		int idx = len - 1;// 새로운 데이터의 위치정보를 수시로 이 변수에 업뎃할것임
-		while (idx > 1) { // 루트노드가 아닐때만
-			if (data[idx] < data[idx / 2]) {
-				int temp = data[idx];
-				data[idx] = data[idx / 2];
-				data[idx / 2] = temp;
-				idx = idx / 2; // 자리바꾸면 업뎃!
-			}
-			else 
-				break;
+		data[len++] = x;//마지막 노드에 newData 추가
+		int idx = len - 1;//옮겨질 새로운 데이터의 위치정보를 수시로 이 변수에 업뎃할것임
+		while (idx > 1 && data[idx] < data[idx / 2]) { //루트노드가 아닐때만
+			int temp = data[idx];
+			data[idx] = data[idx / 2];
+			data[idx / 2] = temp;
+			idx = idx / 2; // 자리바꾸면 업뎃!
 		}
 	}
 
@@ -56,11 +52,8 @@ struct priorityQueue {
 		len--;
 
 		int idx = 1;
-		while (1) {
+		while (1) {//현재 idx의 위치에서 자식들과 교체할 수 있는지 비교
 			int pIdx = -1;// 우선순위가 높은 친구의 노드번호
-			
-			//0. (중요) 왼쪽, 오른쪽 자식들중에 존재하는 것
-			
 			//(A) 자식이 모두 없는 경우
 			if (idx * 2 >= len)
 				break;
@@ -78,7 +71,7 @@ struct priorityQueue {
 					pIdx = idx * 2 + 1;       
 			}
 			//부모(idx)의 자식노드 번호가 pIdx에 저장됨..
-			
+			//pIdx를 찾는 과정이 길기 때문에 while문의 조건절을 true로 해줘야 함.
 			//2. 나와 그 우선순위가 높은 친구를 비교해서 자리를 바꾸어야함.
 			if (data[idx] > data[pIdx]) {
 				int temp = data[idx];
